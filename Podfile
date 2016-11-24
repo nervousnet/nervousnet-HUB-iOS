a659f6e5d34d0477ca991b5132b826f269bd97ab
Andreas Enz <andi_enz@msn.com>
2016-11-24 16:54:57 +0100

    Added some PODS: most importantly a good Logger class which you should use from now on. I added a link to the wiki which should explain the basics

---
+++ Podfile
@@ -0,0 +1,10 @@
+
+target 'nervousnet-HUB-iOS' do
+  use_frameworks!
+
+  # Pods for nervousnet-HUB-iOS
+  pod 'Swifter', '~> 1.3.2'
+  pod 'Zip', '~> 0.2'
+  pod 'SwiftyJSON', '~> 3.1.3'
+  pod 'XCGLogger', '~> 4.0.0'
+end
--- nervousnet-HUB-iOS/AppDelegate.swift
+++ nervousnet-HUB-iOS/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import XCGLogger
+
+let log = XCGLogger.default
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        log.setup(level: .debug, showThreadName: true, showLevel: true, showFileNames: true, showLineNumbers: true, writeToFile: "logs/debug.log", fileLevel: .debug)
+        
         return true
     }
 
