a33d26a23596b8ea82a8a67dfebeeed04960eb97
Andreas Enz <andi_enz@msn.com>
2017-03-10 11:49:09 +0100

    small project config changes to get UnitTests working

--- nervousnet-HUB-iOSTests/nervousnet_HUB_iOSTests.swift
+++ nervousnet-HUB-iOSTests/nervousnet_HUB_iOSTests.swift
@@ -5,6 +5,20 @@
 //  Created by prasad on 21/09/16.
 //  Copyright © 2016 ETH Zurich - COSS. All rights reserved.
 //
+/* The basic structure of a test function is as follows:
+ * - the name of the function needs to have prefix 'test'
+ * - the function takes no parameters and returns void
+ * - compute something
+ * - check the result of computation against expected result using assertion:
+ *   see 'Assertions Listed by Category' on 
+ *   https://developer.apple.com/library/content/documentation/DeveloperTools/Conceptual/testing_with_xcode/chapters/04-writing_tests.html
+ *
+ * example:
+ * func testExample() {
+ *      let value = testSomething()
+ *      XCTAssertEquals(value, 5)
+ * }
+ */
 
 import XCTest
 @testable import nervousnet_HUB_iOS
@@ -24,6 +38,12 @@ class nervousnet_HUB_iOSTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
+    }
+    
+    
+    func testDB() { // test functions need to have prefix 'test', take no parameters, and return void
+        
     }
     
     func testPerformanceExample() {
