6f94a82b0d788199f8099177a712ab7b0ce8d131
a-enz <andi_enz@msn.com>
2017-05-22 19:02:46 +0200

    Added basic version of About Screen.
    The buttons still need to be linked and a popup for the terms and conditions created

---
+++ nervousnet-HUB-iOS/AboutScreenViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AboutScreenViewController.swift
+//  nervousnet-HUB-iOS
+//
+//  Created by Enz  Andreas on 22.05.17.
+//  Copyright © 2017 ETH Zurich - COSS. All rights reserved.
+//
+
+import UIKit
+
+class AboutScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func openAppRating() {
+        //TODO: open apple store, use storeproductviewcontroller?
+    }
+    
+    //TODO: show popup with terms of use
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
